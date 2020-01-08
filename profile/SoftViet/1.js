d751b09b6aad69718e9979f716a7ce1e805d2974
frontend99 <coderfrontendvn@gmail.com>
2020-01-08 14:49:15 +0700

    update giao dien

---
+++ profile/SoftViet/1.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	var ctm_left = document.querySelector('.ctm_left');
+	var ctm_right = document.querySelector('.ctm_right');
+	var audio_left = document.querySelector('.audio_left');
+	var audio_right = document.querySelector('.audio_right');
+
+	// js customer
+	window.addEventListener('scroll',function(){
+		if(window.pageYOffset > 1200){
+			ctm_left.classList.add('move_right');
+			ctm_right.classList.add('move_left');
+		}
+		if(window.pageYOffset > 2200){
+			audio_left.classList.add('move_right');
+			audio_right.classList.add('move_left');
+		}
+	})
+	// end js customer
+	
+},false)
