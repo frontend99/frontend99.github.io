bf25ae2912bb67a47c061862fbd138735e198888
frontend99 <coderfrontendvn@gmail.com>
2020-01-10 16:09:23 +0700

    version 3.0

---
+++ js/app.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+    var divPress = document.querySelector('.press'),
+    divContent = document.querySelector('.content'),
+    divCrossbar =  document.querySelector('.crossbar'),
+    pCLick =  document.querySelector('.pClick'),
+    h3 = document.querySelector('h3'),
+    statusButton = 0;
+
+    divPress.addEventListener('click', function(){
+        divPress.classList.toggle('press-down');
+        h3.classList.toggle('h3-hide');
+        divContent.classList.toggle('scale-full');
+        divCrossbar.classList.toggle('style-crossbar');
+        if(statusButton == 0){
+            pCLick.innerHTML = "Home";
+            statusButton = 1;
+        }
+        else if(statusButton == 1){
+            pCLick.innerHTML = "Click";
+            statusButton = 0;
+        }
+        console.log(statusButton);
+        
+    })
+},false)
